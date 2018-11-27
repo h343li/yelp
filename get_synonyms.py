ebbb38184818d36dba738659b48dce73a345963f
Yan Yu Chen <Amy@Yans-MacBook-Pro.local>
2018-11-26 19:53:57 -0500

    get_synonyms python file

---
+++ get_synonyms.py
@@ -0,0 +1,16 @@
+import nltk.corpus import wordnet as wn
+
+synonyms = []
+antonyms = []
+
+for syn in wn.synsets('fast'):
+#syn = wn.synsets('fast')
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+    for s in syn.similar_tos():
+        synonyms.append(s.name().split('.')[0])
+
+
+print(set(synonyms), set(antonyms))
